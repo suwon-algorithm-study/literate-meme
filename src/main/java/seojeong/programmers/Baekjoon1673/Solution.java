e581c08418a6c9519843579017b2e09ae7aa1dfb
김서짱 <seojeong1101@gmail.com>
2022-03-20 12:13:01 +0900

    22.03.20

---
+++ src/main/java/seojeong/programmers/Baekjoon1673/Solution.java
@@ -0,0 +1,32 @@
+package seojeong.programmers.Baekjoon1673;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 쿠폰
+        int k = Integer.parseInt(st.nextToken()); // 도장
+
+        int result = 0;
+        result += n;
+
+        int res = 0;
+
+        while(n >= k) {
+            result += n/k;
+            res += n%k;
+            n /= k;
+            System.out.println("남은 도장 개수"+res);
+        }
+
+        System.out.println(result);
+
+    }
+}
+
---
+++ src/main/java/seojeong/programmers/Baekjoon2455/Solution.java
@@ -0,0 +1,25 @@
+package seojeong.programmers.Baekjoon2455;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int max = 0;
+        int count = 0;
+
+        for(int i=0; i<4; i++){
+            st = new StringTokenizer(br.readLine());
+            count -= Integer.parseInt(st.nextToken());
+            count += Integer.parseInt(st.nextToken());
+            max = Math.max(max,count);
+        }
+        System.out.println(max);
+
+    }
+}
+*/
